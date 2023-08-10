9fce0593e250e96294252ab4d3f0819cb402d119
Elizaveta Anokhova <liza.ru96@mail.ru>
2023-08-10 16:55:17 +0300

    URA

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int balance = 89;  // рублей текущий баланс
+        int refill = 1000;
+
+        if (refill > 1000) {
+            System.out.println((refill / 100) + " рублей бонусами");
+            System.out.printf((refill / 100 + refill + balance) + " рублей - итоговая сумма на балансе");
+        } else {
+            System.out.println((0) + " рублей бонусами");
+            System.out.printf((refill + balance) + " рублей - итоговая сумма на балансе");
+
+    }
+}}
